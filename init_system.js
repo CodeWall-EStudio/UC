41e01f312aef1304d95641c49ca183e83eeb7208
azrael <azrael@imatlas.com>
2014-12-13 15:17:37 +0800

    up

--- init_system.js
+++ init_system.js
@@ -4,8 +4,8 @@ db.OAuthClients.create({
     clientId: '54321',
     clientSecret: '22ff294ea9c161ca843fbc22eafb068b',
     redirectUri: 'http://szone.hylc-edu.cn/api/login/loginUCSuccess',
-    allowGrantTypes: ['usercenter'],
-    authType: 'token',
+    allowGrantTypes: ['password'],
+    authType: 'usercenter',
     name: '工作室'
 }, function(err) {
     console.log('inited with error: ', err);
